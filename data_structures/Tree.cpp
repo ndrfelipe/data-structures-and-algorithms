fe6353149531952b42f1001173f7073e7aeb66f9
ndrfelipe <ndrfelipebraga@gmail.com>
2025-09-17 21:39:57 -0300

    desafio feito

--- data_structures/Tree.cpp
+++ data_structures/Tree.cpp
@@ -32,37 +32,59 @@ Node* insert(Node* node, int value){
 }
 
 Node* search(Node* node, int value){
-    if (node == nullptr){
-        std::cout<<"Node don't found. "<<"\n";
+    if (node == nullptr || node->value == value){
+        if(node != nullptr){
+            std::cout<< "\n Found: " << node->value << "\n";
+        }else{
+            std::cout<<"Node don't found. "<<"\n";
+        }
         return node;
     }
 
     if(value < node->value){
-        node->left = search(node->left, value);
-    }else if(value > node->value){
-        node->right = search(node->right, value);
-    }else{
-        std::cout<<"\nNode found: "<< node->value<<"\n";
+        return search(node->left, value);
+    }
+
+    return search(node->right, value);
+}
+
+Node* findMinimalLeftValue(Node* node){
+    while(node->left != NULL){
+        node = node->left;
     }
     return node;
 }
 
-int main(){
-    Node* node = nullptr;
+Node* remove(Node* node, int value){
+    if (node == nullptr){
+        return node;
+    }
 
-    node = insert(node, 50);
-    insert(node, 30);
-    insert(node, 70);
-    insert(node, 20);
-    insert(node, 40);
-    insert(node, 60);
-    insert(node, 80);
+    if(value < node->value){
+        node->left = remove(node->left, value);
+    }else if(value > node->value){
+        node->right = remove(node->right, value);
+    }else{
 
-    search(node, 40);
-    search(node, 1);
-    delete(node);
+        if(node->left == NULL && node->right == NULL){
+            delete(node);
+            return NULL;
+        }
 
-    return 0;
+        if(node->left == NULL){
+            Node* temp = node->right;
+            delete(node);
+            return temp;
+        }else if (node->right == NULL){
+            Node* temp = node->left;
+            delete(node);
+            return temp;
+        }
+        Node* temp = findMinimalLeftValue(node->right);
+        node->value = temp->value;
+        node->right = remove(node->right, temp->value);
+    }
+    return node;
 }
 
 
@@ -91,3 +113,89 @@ void posOrder(Node* node){
         std::cout<<node->value<<", ";
     }
 }
+
+void deleteTree(Node* node){
+    if (node == nullptr){
+        return;
+    }
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
+
+int main(){
+    Node* node = nullptr;
+    int input, option;
+    bool state = true;
+
+    while (state)
+    {
+        std::cout << "Menu de navegação\n";
+        std::cout << "1 - Inserir valor\n";
+        std::cout << "2 - Remover valor\n";
+        std::cout << "3 - Buscar valor\n";
+        std::cout << "4 - Exibir em ordem\n";
+        std::cout << "5 - Sair\n";
+
+        std::cout << "Digite o valor que deseja: ";
+        std::cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            std::cout << "\nDigite o valor que deseja inserir: ";
+            std::cin >> input;
+
+            std::cout << "\nInserindo nó << " << input << "... \n";
+            node = insert(node, input);
+            break;
+        case 2:
+            if (node == NULL){
+                std::cout << "\n Para remover você precisa inserir um valor antes. \n";
+                break;
+            }
+            std::cout << "\nDigite o valor que deseja remover: ";
+            std::cin >> input;
+
+            std::cout << "\nRemovendo nó << " << input << "... \n";
+            node = remove(node, input);
+            break;
+
+        case 3:
+            if (node == NULL){
+                std::cout << "\n Para buscar você precisa inserir um valor antes. \n";
+                break;
+            }
+            std::cout << "\nDigite o valor que deseja buscar: ";
+            std::cin >> input;
+
+            std::cout << "\nBuscando nó << " << input << "... \n";
+            search(node, input);
+            break;
+        case 4:
+            if (node == NULL){
+                std::cout << "\n Para exibir você precisa inserir um valor antes. \n";
+                break;
+            }
+            std::cout << "\n Exibindo em ordem... \n";
+            inOrder(node);
+            break;
+        case 5:
+            std::cout << "\n Saindo... \n";
+            state = false;
+            break;
+            
+        
+        default:
+            std::cout << "\n Opção inválida! Tente novamente... \n";
+            continue;;
+        }
+
+    }
+    
+    
+    deleteTree(node);
+    return 0;
+}
+
+
